41e6f8590831be667fa4be76b1a5301e9c8a1243
DSGeorgiev <65136753+dstgeorgiev@users.noreply.github.com>
2021-03-16 12:32:20 +0000

    working player healthbar

---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradientColour;
+    public Image fill;
+
+    public void SetHealt(int health)
+    {
+        slider.value = health;
+        fill.color = gradientColour.Evaluate(slider.normalizedValue);
+    }
+
+    public void SetMaxHelth(int maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+        fill.color = gradientColour.Evaluate(1f);
+
+
+    }
+}
--- Assets/Scripts/PlayerHurtbox.cs
+++ Assets/Scripts/PlayerHurtbox.cs
@@ -6,18 +6,22 @@ public class PlayerHurtbox : MonoBehaviour
 {
     public int maxHealth = 100;
     int currentHealth;
+    public PlayerHealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHelth(maxHealth);
     }
 
     //this method is called 
     public void TakeHit(int damage)
     {
         currentHealth -= damage;
+        healthBar.SetHealt(currentHealth);
         PlayerControl player = GetComponentInParent<PlayerControl>();
+
 
         player.GetHurt();
         if (currentHealth <= 0)
