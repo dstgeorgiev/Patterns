eebaae9265e7498580c5d368cfd00a7fcbdd0cea
DSGeorgiev <65136753+dstgeorgiev@users.noreply.github.com>
2021-03-21 14:05:46 +0000

    level 2 props finished

--- Assets/Scripts/EnemyHurtbox.cs
+++ Assets/Scripts/EnemyHurtbox.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyHurtbox : MonoBehaviour
 {
-    public int maxHealth = 100;
+    public int maxHealth = 80;
     int currentHealth;
     public HealthBar healthBar;
 
