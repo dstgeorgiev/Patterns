14662af3c96826642cae1017bf989f7b0b153cdd
DSGeorgiev <65136753+dstgeorgiev@users.noreply.github.com>
2021-03-28 00:21:55 +0000

    platforms in Forest level ready

---
+++ Assets/Scripts/ParalaxBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxBackground : MonoBehaviour
+{
+    [SerializeField] private Vector2 paralaxEffectM;
+
+
+    private Transform cameraTransform;
+    private Vector3 lastPositionCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastPositionCamera = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastPositionCamera;
+        transform.position += new Vector3(deltaMovement.x * paralaxEffectM.x, deltaMovement.y * paralaxEffectM.y);
+        lastPositionCamera = cameraTransform.position;
+    }
+}
