d9fc4c7f9f9afa82998461aa88e84d5124e7b6cb
DSGeorgiev <65136753+dstgeorgiev@users.noreply.github.com>
2021-03-31 03:07:27 +0100

    narrator done - scene 1

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameTxt;
+    public TextMeshProUGUI dialogueTxt;
+    public GameObject whoSpeaks;
+    
+
+    private Queue<string> sentences;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameTxt.text = dialogue.name;
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        ShowNextSentence();
+    }
+
+    public void ShowNextSentence()
+    {
+        if(sentences.Count==0)
+        {
+            //the dialogue ended
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueTxt.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueTxt.text = "";
+        foreach (char character in sentence.ToCharArray())
+        {
+            dialogueTxt.text += character;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        Time.timeScale = 1f;
+        whoSpeaks.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject whoSpeaks;
+    public Dialogue dialogue;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            TriggerDialogue();
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        whoSpeaks.SetActive(true);
+        Time.timeScale = 0f;
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
